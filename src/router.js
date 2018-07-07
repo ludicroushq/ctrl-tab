c5ef3d4310cdedb9f9dd2163c85d5e81dc89d654
Manthan Mallikarjun <nahtnam@gmail.com>
2018-07-07 16:02:57 -0700

    remove boilerplate

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import About from './views/About.vue';
 
 Vue.use(Router);
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
+    }
   ],
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello Chrome!
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
+  name: 'Home',
 };
 </script>
