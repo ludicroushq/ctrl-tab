5c76d2a95462f6d3fc585ca3971c66d8d1397fb5
Manthan Mallikarjun <nahtnam@gmail.com>
2018-08-02 00:31:42 -0700

    move spinner to its own component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,35 @@
+<template>
+  <button :class="{
+    button: true,
+    'is-loading': true,
+    'spinner': true,
+    'is-large': !(isSmall),
+  }">Loading</button>
+</template>
+
+<script>
+export default {
+  props: ['isSmall'],
+};
+</script>
+
+
+<style lang="scss" scoped>
+.spinner {
+  border: 0px;
+  background: rgba(0, 0, 0, 0);
+}
+
+button.is-loading.spinner::after {
+  animation: spinAround 500ms infinite linear;
+  border: 2px solid #3f51b5;
+  border-radius: 290486px;
+  border-right-color: transparent;
+  border-top-color: transparent;
+  content: "";
+  display: block;
+  height: 1em;
+  position: relative;
+  width: 1em;
+}
+</style>
--- src/components/message/MessageItem.vue
+++ src/components/message/MessageItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-if="isLoading" class="has-text-centered">
-      <button class="button is-loading spinner is-large">Loading</button>
+      <Spinner />
     </div>
     <div v-else v-for="item in this.data" :key="item.id">
       <div class="item">
@@ -23,28 +23,13 @@
 </template>
 
 <script>
+import Spinner from '@/components/Spinner.vue';
+
 export default {
   name: 'Message-Item',
   props: ['data', 'isLoading'],
+  components: {
+    Spinner,
+  },
 };
 </script>
-
-<style lang="scss" scoped>
-.spinner {
-  border: 0px;
-  background: rgba(0, 0, 0, 0);
-}
-
-button.is-loading.spinner::after {
-  animation: spinAround 500ms infinite linear;
-  border: 2px solid #3f51b5;
-  border-radius: 290486px;
-  border-right-color: transparent;
-  border-top-color: transparent;
-  content: "";
-  display: block;
-  height: 1em;
-  position: relative;
-  width: 1em;
-}
-</style>
