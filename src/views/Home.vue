cf85ce78ce3eeeb782786dffa3e3d90ae6bf7dae
Manthan Mallikarjun <nahtnam@gmail.com>
2018-09-17 01:59:09 -0700

    add sorting and settings page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,28 @@
           <button class="delete" aria-label="close" @click="closeSettingsModal"></button>
         </header>
         <section class="modal-card-body">
-          Settings coming soon!
+          <h1 class="title">Select Providers</h1>
+          <h2 class="subtitle">Up to 4. Drag on the homepage to reorder them.</h2>
+          <div class="subtitle is-marginless" v-if="this.providers.length >= 4">
+            Unselect One to Continue
+          </div>
+          <div class="subtitle is-marginless" v-else-if="this.providers.length <= 3">
+            Select One to Continue
+          </div>
+          <hr>
+          <div class="field" v-for="provider in this.allProviders" :key="provider.component">
+            <div class="control">
+              <label class="checkbox">
+                <input
+                  type="checkbox"
+                  @click="toggleProvider(provider.component)"
+                  :disabled="isDisabled(provider.component)"
+                  :checked="isChecked(provider.component)"
+                >
+                {{ provider.name }}
+              </label>
+            </div>
+          </div>
         </section>
         <footer class="modal-card-foot">
           <button class="button is-success" @click="closeSettingsModal">Close</button>
@@ -19,25 +40,27 @@
       icon-size="small"
       bg-color="#363636"
       z-index="30"
+      main-tooltip="Drag the Panels to Reoder<br>Click to Configure"
       :actions="fabActions"
       @settings="openSettingsModal"
     ></fab>
-    <div class="columns">
+    <draggable class="columns" :list="this.providers" @end="onEnd">
       <div
         class="column"
         v-for="provider in this.providers"
         :key="provider.title"
-        v-if="provider.selected"
       >
         <component :is="provider.component"></component>
       </div>
-    </div>
+    </draggable>
   </section>
 </template>
 
 <script>
 import fab from 'vue-fab';
+import draggable from 'vuedraggable';
 
+import { getData, storeData } from '@/utils/local-storage';
 import HackerNews from '@/components/providers/HackerNews.vue';
 import ProductHunt from '@/components/providers/ProductHunt.vue';
 import Github from '@/components/providers/Github.vue';
@@ -45,31 +68,30 @@ import Reddit from '@/components/providers/Reddit.vue';
 import NewYorkTimes from '@/components/providers/NewYorkTimes.vue';
 // import Todoist from '@/components/providers/Todoist.vue';
 
-const providers = [
+const allProviders = [
   {
     name: 'Reddit',
     component: 'Reddit',
-    selected: true,
+    default: true,
   },
   {
     name: 'Product Hunt',
     component: 'ProductHunt',
-    selected: true,
+    default: true,
   },
   {
     name: 'Hacker News',
     component: 'HackerNews',
-    selected: true,
+    default: true,
   },
   {
     name: 'Github',
     component: 'Github',
-    selected: true,
   },
   {
     name: 'New York Times',
     component: 'NewYorkTimes',
-    selected: true,
+    default: true,
   },
   // {
   //   name: 'Todoist',
@@ -89,19 +111,50 @@ export default {
     NewYorkTimes,
     // Todoist,
     fab,
+    draggable,
   },
 
   methods: {
     openSettingsModal() {
       this.settingsModal = true;
     },
+
     closeSettingsModal() {
       this.settingsModal = false;
+    },
+
+    onEnd() {
+      storeData('providers', this.providers);
+    },
+
+    toggleProvider(component) {
+      const provider = this.providers.find(obj => obj.component === component);
+      if (!provider) {
+        this.providers.push(this.allProviders.find(obj => obj.component === component));
+        return storeData('providers', this.providers);
+      }
+      this.providers = this.providers.filter(obj => obj.component !== component);
+      return storeData('providers', this.providers);
+    },
+
+    isChecked(component) {
+      return this.providers.find(obj => obj.component === component);
+    },
+
+    isDisabled(component) {
+      return (!this.isChecked(component) && this.providers.length >= 4) ||
+        (this.isChecked(component) && this.providers.length <= 3);
     },
   },
 
   data() {
+    let providers = getData('providers');
+    if (!providers) {
+      providers = allProviders.filter(provider => provider.default);
+      storeData('providers', providers);
+    }
     return {
+      allProviders,
       providers,
       settingsModal: false,
       fabActions: [
