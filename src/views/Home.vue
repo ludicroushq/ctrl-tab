182fd41d013c495d648d63977522a12e05751a32
Manthan Mallikarjun <nahtnam@gmail.com>
2018-09-17 02:18:47 -0700

    update settings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
         <section class="modal-card-body">
           <h1 class="title">Select Providers</h1>
           <h2 class="subtitle">Up to 4. Drag the panels on the homepage to reorder them.</h2>
-          <div class="subtitle is-marginless" v-if="this.providers.length >= 4">
+          <div class="subtitle is-marginless" v-if="this.providers.length >= 5">
             Unselect One to Continue
           </div>
           <div class="subtitle is-marginless" v-else-if="this.providers.length <= 1">
@@ -142,7 +142,7 @@ export default {
     },
 
     isDisabled(component) {
-      return (!this.isChecked(component) && this.providers.length >= 4) ||
+      return (!this.isChecked(component) && this.providers.length >= 5) ||
         (this.isChecked(component) && this.providers.length <= 1);
     },
   },
