1268352a574ced6a98ed9cf391ae3eacb9d8e17e
Manthan Mallikarjun <nahtnam@gmail.com>
2018-12-16 15:17:07 -0800

    update firefox publish

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -58,7 +58,7 @@ const exec = async () => {
   await publish();
   console.log('> Uploading and signing new version to Firefox');
   try {
-    childProcess.execSync('./node_modules/.bin/web-ext-submit', { stdio: [0, 1, 2], cwd: distPath });
+    childProcess.execSync('npm run publish:firefox', { stdio: [0, 1, 2], cwd: distPath });
   } catch (_) {
     // do nothing
   }
