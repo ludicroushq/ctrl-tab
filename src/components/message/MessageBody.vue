1c1e99918c5d5479bed81d0a4b831afed15587e4
Manthan Mallikarjun <nahtnam@gmail.com>
2019-01-05 00:50:41 -0800

    remove global scrollbar and remove delay

--- src/components/message/MessageBody.vue
+++ src/components/message/MessageBody.vue
@@ -15,13 +15,11 @@ export default {
 <style lang="scss" scoped>
 .message-body {
   visibility: hidden;
-  transition: visibility 0s 5s;
 }
 
 .message-content,
 .message-body:hover,
 .message-body:focus {
   visibility: visible;
-  transition: visibility 0.2s;
 }
 </style>
