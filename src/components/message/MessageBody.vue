ae1a4a2456564404f384f7b49b2cba07a75ec6c6
Manthan Mallikarjun <nahtnam@gmail.com>
2019-01-04 22:41:57 -0800

    hide scrollbar until hover

--- src/components/message/MessageBody.vue
+++ src/components/message/MessageBody.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="message-body">
-    <slot></slot>
+    <div class="message-content">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -9,3 +11,17 @@ export default {
   name: 'Message-Body',
 };
 </script>
+
+<style lang="scss" scoped>
+.message-body {
+  visibility: hidden;
+  transition: visibility 0s 5s;
+}
+
+.message-content,
+.message-body:hover,
+.message-body:focus {
+  visibility: visible;
+  transition: visibility 0.2s;
+}
+</style>
